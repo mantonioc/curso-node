2fac3f6b98aa244c6d331b1fcc7e6e5e3aa8f1b0
= <mantonio.c@gmail.com>
2017-04-29 18:45:08 -0300

    Primeiro commit - Início do projeto

---
+++ projeto1/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/',function(req,res){
+	res.render('home/index');
+});
+app.get('/formulario_inclusao_de_noticia',function(req,res){
+	res.render('admin/form_add_noticia');
+});
+app.get('/noticias',function(req,res){
+	res.render('noticias/noticias');
+});
+
+app.listen(3000, function(){
+	console.log('teste 8 -  express');
+});
---
+++ projeto1/main.js
@@ -0,0 +1,14 @@
+var http = require('http');
+	
+var server = http.createServer( function(req,res){
+
+	var page = req.url;
+
+	if(page == '/tecnologia')
+		res.end('<html><body>Teste de servidor WEB nodeJS - Tecnologia</body></html>');
+	else
+		res.end('<html><body>Teste de servidor WEB nodeJS</body></html>');
+
+} );
+
+server.listen(3000);
