de0c63243dfd82806a07d2e153911bf11191690e
= <mantonio.c@gmail.com>
2017-04-30 23:37:38 -0300

    Implementação de modularização do projeto

--- projeto1/app.js
+++ projeto1/app.js
@@ -1,18 +1,9 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server');
 
-app.set('view engine', 'ejs');
-
-app.get('/',function(req,res){
-	res.render('home/index');
-});
-app.get('/formulario_inclusao_de_noticia',function(req,res){
-	res.render('admin/form_add_noticia');
-});
-app.get('/noticias',function(req,res){
-	res.render('noticias/noticias');
-});
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaHome = require('./app/routes/home')(app);
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
-	console.log('teste 8 -  express');
+	console.log('Servidor ON');
 });
