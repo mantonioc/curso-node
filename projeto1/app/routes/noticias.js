ad05870302c52bfe5b8b1ef1e9ca7162852d57e7
= <mantonio.c@gmail.com>
2017-05-27 23:39:53 -0300

    Iplementação do módulo consign

--- projeto1/app/routes/noticias.js
+++ projeto1/app/routes/noticias.js
@@ -1,20 +1,14 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app) {
 
+	var connection = dbConnection();
+
 	app.get('/noticias',function(req,res){
-
-		var mysql = require('mysql');
-		var connection = mysql.createConnection({
-			host : 'localhost',
-			user : 'root',
-			password : '',
-			database : 'portal_noticias'
-		});
-
 		connection.query('SELECT * FROM noticias', function(error,result){
-			res.send(result);
+			//res.send(result);
+			res.render('noticias/noticias',{noticias : result});
 		});
-
-		//res.render('noticias/noticias');
 	});
 
 };
